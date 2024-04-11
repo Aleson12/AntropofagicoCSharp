e3e6d35330ee7c2c05b1c692f9bdf9ee791222b7
Aleson Sobral Felizardo <felizardoaleson@yahoo.com>
2024-04-11 16:29:49 -0300

    comentando o código

--- AntropofagicoCSharp/Classes/Antropofagico.cs
+++ AntropofagicoCSharp/Classes/Antropofagico.cs
@@ -1,23 +1,29 @@
-﻿using System;
+﻿
+// importação das bibliotecas necessárias:
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Data.Analysis;
+using Microsoft.Data.Analysis; // biblioteca para trabalhar com DataFrame
 using Microsoft.ML;
 
 namespace AntropofagicoCSharp.Classes
 {
     internal class Antropofagico
     {
-
-        private int dados;
+        
+        // declaração de listas:
 
         List<string> Lista_de_arquivos_txt_da_pasta = new List<string>();
         List<string> Lista_dos_arquivos_agrupados = new List<string>();
         List<int> Media_dos_valores_da_matriz = new List<int>();
-        List<string> List_nomecsv = new List<string>(); 
-        
+        List<string> List_nomecsv = new List<string>();
+
+        // variáveis globais:
+
+        private int dados;
         int DivisorParaMediaEQuantidadeDeColunasDaMatriz = 0;
         string Caminho_com_o_nome_do_arquivo_csv_final = "";
         string Pasta_dos_arquivos_csv_pos_tratamento = "";

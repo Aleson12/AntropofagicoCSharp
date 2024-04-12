df6600fee3b3b5bd7afbb3051e5ebe959f7a89ef
Aleson Sobral Felizardo <felizardoaleson@yahoo.com>
2024-04-12 12:48:00 -0300

    Mudança de planos e início da criação das interfaces

--- AntropofagicoCSharp/Forms/Form1.Designer.cs
+++ AntropofagicoCSharp/Forms/Form1.Designer.cs
@@ -28,12 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            label4 = new Label();
+            groupBox2 = new GroupBox();
+            label2 = new Label();
+            maskedTextBox1 = new MaskedTextBox();
+            label1 = new Label();
+            label3 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = SystemColors.ButtonFace;
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(groupBox2);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(maskedTextBox1);
+            groupBox1.ForeColor = Color.LimeGreen;
+            groupBox1.Location = new Point(21, 112);
+            groupBox1.Margin = new Padding(5, 6, 5, 6);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(3, 4, 3, 4);
+            groupBox1.Size = new Size(525, 402);
+            groupBox1.TabIndex = 3;
+            groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Green;
+            button1.Cursor = Cursors.Hand;
+            button1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.BlanchedAlmond;
+            button1.Location = new Point(21, 348);
+            button1.Name = "button1";
+            button1.Size = new Size(482, 47);
+            button1.TabIndex = 7;
+            button1.Text = "Abrir";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.Black;
+            label4.Location = new Point(177, 92);
+            label4.Name = "label4";
+            label4.Size = new Size(164, 21);
+            label4.TabIndex = 6;
+            label4.Text = "Conteúdo do diretório";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Location = new Point(21, 116);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(482, 214);
+            groupBox2.TabIndex = 4;
+            groupBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Black;
+            label2.Location = new Point(6, 35);
+            label2.Name = "label2";
+            label2.Size = new Size(72, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Diretório:";
+            label2.Click += label2_Click;
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(84, 32);
+            maskedTextBox1.Margin = new Padding(3, 4, 3, 4);
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(419, 23);
+            maskedTextBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(163, 85);
+            label1.Name = "label1";
+            label1.Size = new Size(240, 21);
+            label1.TabIndex = 2;
+            label1.Text = "Leitura | Exibição dos arquivos txt";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Click += label1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(466, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(345, 23);
+            label3.TabIndex = 3;
+            label3.Text = "Análise de Componentes Principais";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 18F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1271, 743);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(groupBox1);
+            Font = new Font("Sitka Text", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private GroupBox groupBox1;
+        private Label label1;
+        private MaskedTextBox maskedTextBox1;
+        private Label label2;
+        private GroupBox groupBox2;
+        private Button button1;
+        private Label label4;
+        private Label label3;
     }
 }
--- AntropofagicoCSharp/Forms/Form1.cs
+++ AntropofagicoCSharp/Forms/Form1.cs
@@ -1,10 +1,66 @@
+
+// importação das bibliotecas necessárias:
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Analysis; // biblioteca para trabalhar com DataFrame
+using Microsoft.ML;
+
 namespace AntropofagicoCSharp
+
 {
     public partial class Form1 : Form
     {
+        // declaração de listas:
+
+        List<string> Lista_de_arquivos_txt_da_pasta = new List<string>();
+        List<string> Lista_dos_arquivos_agrupados = new List<string>();
+        List<string> Lista_nomecsv = new List<string>();
+
+        // declaração de um vetor:
+
+        double[,] Media_dos_valores_da_matriz = new double[,] { };
+
+        // variáveis globais:
+
+        int dados;
+        int DivisorParaMediaEQuantidadeDeColunasDaMatriz = 0;
+        string Caminho_com_o_nome_do_arquivo_csv_final = "";
+        string Pasta_dos_arquivos_csv_pos_tratamento = "";
+        string Numero_pos_hifen = "";
+        string Compara_numero = "";
+        string Compara_nome = "";
+        string Nome_com_tipo = "";
+        string Nome_do_csv = "";
+        string Nome_novo = "";
+        string Diretorio = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }

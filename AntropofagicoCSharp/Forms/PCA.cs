6c6f36308a4984571cc272ac900469e6011f3a5d
Aleson Sobral Felizardo <felizardoaleson@yahoo.com>
2024-06-18 16:03:11 -0300

    criado o gráfico de dispersão

--- AntropofagicoCSharp/Forms/PCA.cs
+++ AntropofagicoCSharp/Forms/PCA.cs
@@ -13,22 +13,19 @@ namespace AntropofagicoCSharp.Forms
 
         private void PlotPCAresultados(List<double> elementosDaPrimeiraColuna, List<double> elementosDaSegundaColuna)
         {
+             Chart chart = this.chart1; // acessando a interface do gráfico
 
+             Series scatterSeries = chart.Series.Add("Dados dispersos"); // adiciona uma série de dados à interface do gráfico
+             // e também um identificador único a esse conjunto de dados ("Dados dispersos"), e o insere na variável 
+             // "scaterSeries"
 
-            Chart chart = this.chart1; // acessando a interface do gráfico
+             scatterSeries.ChartType = SeriesChartType.FastPoint; // define o tipo de gráfico (de pontos, no caso)
+             
+            foreach (int i in Enumerable.Range(0, Math.Min(elementosDaPrimeiraColuna.Count, elementosDaSegundaColuna.Count)))
+                scatterSeries.Points.AddXY(elementosDaPrimeiraColuna[i], elementosDaSegundaColuna[i]);
 
-            Series scatterSeries = chart.Series.Add("Dados dispersos"); // adiciona uma série de dados à interface do gráfico
-            // e também um identificador único a esse conjunto de dados ("Dados dispersos"), e o insere na variável 
-            // "scaterSeries"
-
-            scatterSeries.ChartType = SeriesChartType.Point; // define o tipo de gráfico (de pontos, no caso)
-
-            elementosDaSegundaColuna.ForEach(valorSgndaColuna =>
-                scatterSeries.Points.AddY(valorSgndaColuna));
-
-            scatterSeries.MarkerStyle = MarkerStyle.Circle; // define o tipo de ponto (Círculo, no caso)
-            scatterSeries.MarkerSize = 8; // definindo o tamanho de cada ponto
-
+             scatterSeries.MarkerStyle = MarkerStyle.Circle; // define o tipo de ponto (Círculo, no caso)
+             scatterSeries.MarkerSize = 8; // definindo o tamanho de cada ponto
         }
     }
 }
